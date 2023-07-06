e850d448d74e7a395d671eb532bce0cb444e6b3f
NicoNyga <nicolainygaard@me.com>
2023-07-06 18:28:36 +0200

    adding test to playlist component

--- src/components/Playlist/tests/Playlist.test.jsx
+++ src/components/Playlist/tests/Playlist.test.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
-
+import { render, fireEvent } from '@testing-library/react';
 import {Playlist} from '../Playlist';
 
-describe('<Playlist />', () => {});
+describe('<Playlist />', () => {
+test('handles naming of playlist correctly', () => {
+        const { getByPlaceholderText, getByTitle } = render(<Playlist />);
+
+        const input = getByPlaceholderText('Enter playlist name');
+        const saveButton = getByTitle('Remove');
+
+        fireEvent.change(input, { target: { value: 'Playlist'}});
+        fireEvent.click(saveButton);
+
+        expect(input.value).toBe('Playlist');
+
+
+});
+
+
+});
